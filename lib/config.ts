4eb24e0268d18a6ecd74e690088a82b90520b583
Medet Tleukabiluly <m.diddy_11@mail.ru>
2022-06-01 16:34:41 +0600

    fix websocket url

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,4 @@
 const dev = process.env.NODE_ENV !== 'production';
 
 export const serverUrl = dev ? 'http://localhost:3000' : 'https://livecode-kz.herokuapp.com';
+export const websocketUrl = dev ? 'http://localhost:3001' : 'https://livecode-kz.herokuapp.com:3001';
--- pages/hooks/useSocket.ts
+++ pages/hooks/useSocket.ts
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import { io, Socket } from "socket.io-client"
+import { websocketUrl } from "../../lib/config";
 
 const useSocket = (roomId, userId) => {
   const [socket, setSocket] = useState<Socket>()
   useEffect(() => {
-    const _socket = io('http://localhost:3001', {
+    const _socket = io(websocketUrl, {
       auth: {
         roomId,
         userId

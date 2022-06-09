7819a1a6a1815459a79006eb5ae620f94b4e2a27
Medet Tleukabiluly <m.diddy_11@mail.ru>
2022-06-09 19:13:43 +0600

    remove ws server

--- pages/api/socketio.ts
+++ pages/api/socketio.ts
@@ -8,14 +8,79 @@ export const config = {
     bodyParser: false,
   },
 };
+const rooms = new Map();
 
 const handler = async (req: NextApiRequest, res: NextApiResponseServerIO) => {
   if (!res.socket.server.io) {
     // adapt Next's net Server to http Server
     const httpServer: NetServer = res.socket.server as any;
     const io = new ServerIO(httpServer, {
+      cors: {
+        origin: ["http://localhost:3000", "https://livecode.kz", "https://livecode-kz.herokuapp.com"],
+        credentials: true
+      },
       path: "/api/socketio",
     });
+    
+    io.on("connection", (socket) => {
+      const activeUserId = socket.handshake.auth['userId']
+      const activeRoomId = socket.handshake.auth['roomId']
+
+      if (activeRoomId) {
+        socket.join(activeRoomId)
+        if (!rooms.has(activeRoomId)) {
+          rooms.set(activeRoomId, [activeUserId])
+        } else {
+          const members = rooms.get(activeRoomId)
+          rooms.set(activeRoomId, [...members, activeUserId])
+        }
+        io.to(activeRoomId).emit('joined_room', activeUserId)
+      }
+
+      socket.on('input_cursor_position', (data) => {
+        io.to(activeRoomId).except(activeUserId).emit('cursor_position', {
+          position: data,
+          userId: activeUserId
+        })
+      })
+
+      socket.on('input_code_change', (data) => {
+        io.to(activeRoomId).emit('code_change', data)
+      })
+      // socket.on('join_room', () => {
+      //   if (rooms.has(roomId)) {
+      //     const members = rooms.get(roomId)
+      //     rooms.set(roomId, [...members, userId])
+      //   } else {
+      //     rooms.set(roomId, [userId])
+      //   }
+      //   io.to(roomId).emit('joined_room', userId)
+      // })
+
+      socket.on('leave_room', ({ roomId, userId }) => {
+        if (rooms.has(roomId)) {
+          const members = rooms.get(roomId)
+          if (members.includes(userId)) {
+            rooms.set(roomId, members.filter(v => v !== userId))
+          } else {
+            rooms.delete(roomId)
+          }
+        }
+      })
+
+      socket.on('disconnect', () => {
+        io.to(activeRoomId).emit('left_room', activeRoomId)
+        if (rooms.has(activeRoomId)) {
+          const members = rooms.get(activeRoomId)
+          if (members.includes(activeUserId)) {
+            rooms.set(activeRoomId, members.filter(v => v !== activeUserId))
+          } else {
+            rooms.delete(activeRoomId)
+          }
+        }
+      })
+    });
+
     // append SocketIO server to Next.js socket server response
     res.socket.server.io = io;
   }
--- pages/hooks/useSocket.ts
+++ pages/hooks/useSocket.ts
@@ -1,21 +1,47 @@
-import { useEffect, useState } from "react";
-import { io, Socket } from "socket.io-client"
+import { useCallback, useEffect, useState } from "react";
+import { io, Socket, connect } from "socket.io-client"
 import { websocketUrl } from "../../lib/config";
 
 const useSocket = (roomId, userId) => {
   const [socket, setSocket] = useState<Socket>()
-  useEffect(() => {
-    const _socket = io(websocketUrl, {
+
+  const socketInitializer = useCallback(async () => {
+    await fetch('/api/socketio')
+    const _socket = connect(process.env.BASE_URL, {
+      path: "/api/socketio",
       auth: {
         roomId,
         userId
       }
     });
+    // const _socket = io('/api/websocket', {
+    //   auth: {
+    //     roomId,
+    //     userId
+    //   }
+    // })
+    _socket.on('connect', () => {
+      console.log('connected')
+    })
     setSocket(_socket)
-    return () => {
-      _socket.close();
-    }
   }, [roomId, userId])
+
+  useEffect(() => {
+    socketInitializer()
+  }, [socketInitializer])
+
+  // useEffect(() => {
+  //   const _socket = io(websocketUrl, {
+  //     auth: {
+  //       roomId,
+  //       userId
+  //     }
+  //   });
+  //   setSocket(_socket)
+  //   return () => {
+  //     _socket.close();
+  //   }
+  // }, [roomId, userId])
 
   return socket
 }
